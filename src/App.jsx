a2082c92b4605820a7a5878a18f71874c8ebfe98
Andrii Hordiienko <30300588+drew875@users.noreply.github.com>
2025-05-25 16:51:36 +0300

    hw7

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, NavLink } from "react-router-dom";
 import { Suspense, lazy } from "react";
-
+import { Navigation } from "../src/components/Navigations";
 
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 const MoviesDetailsPage = lazy(() => import("./pages/MoviesDetailsPage/MoviesDetailsPage"));
@@ -11,10 +11,7 @@ const MoviePage = lazy(() => import("./pages/MoviesPage"));
 const App = () => {
   return (
     <>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies">Search Movies</NavLink>
-      </nav>
+      <Navigation />
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
---
+++ src/components/Navigations.jsx
@@ -0,0 +1,10 @@
+const Navigation = () => {
+    return (
+        <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/movies">Search Movies</NavLink>
+        </nav>
+    )
+}
+
+export default Navigation;
